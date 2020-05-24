3a268b724cdc7035d8dcde3d013d4e387eade255
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2020-05-24 19:51:16 +0100

    Add more flexibility to the load_pickle file

--- load_pickle.py
+++ load_pickle.py
@@ -1,4 +1,17 @@
 import pickle
+import sys
 
-with open('data/T3_test_data_new.p', 'rb') as f:
-    x = pickle.load(f)
+def openfile(file):
+    with open(file, 'rb') as f:
+        x = pickle.load(f)
+
+    return x
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+
+file = 'data/T3_test_data_new.p' 
+file2 = '/exports/eddie3_homes_local/s1988171/SynthesisEval/synthesis-eval/examples/dot/SketchAdapt'
+x = openfile(file)
+y = openfile(file2)
